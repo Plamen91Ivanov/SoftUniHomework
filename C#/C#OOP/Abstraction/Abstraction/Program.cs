1cd468c054825f3fcbf44fdb9d780717b098a0a1
plamen <plamen91ivanov@abv.bg>
2020-06-02 22:28:14 +0300

    Abs

--- C#/C#OOP/Abstraction/Abstraction/Program.cs
+++ C#/C#OOP/Abstraction/Abstraction/Program.cs
@@ -7,6 +7,8 @@ namespace Abstraction
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            var a = 5;
+            Console.WriteLine(a);
         }
     }
 }

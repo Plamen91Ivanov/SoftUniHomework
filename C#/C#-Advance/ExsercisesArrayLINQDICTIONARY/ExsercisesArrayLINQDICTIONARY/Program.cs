913ecec6bedb19c38dd737d73226ecec224df228
plamen <plamen91ivanov@abv.bg>
2020-06-07 18:50:16 +0300

    LINQ

---
+++ C#/C#-Advance/ExsercisesArrayLINQDICTIONARY/ExsercisesArrayLINQDICTIONARY/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExsercisesArrayLINQDICTIONARY
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            string[] FirstLine = Console.ReadLine().Split(' ');
+
+            string[] SecondLine = Console.ReadLine().Split(' ');
+
+            string[] common = new string[FirstLine.Length];
+
+            int lenght = Math.Min(FirstLine.Length, SecondLine.Length);
+            int countL = 0;
+            int countR = 0;
+
+            for (int i = 0; i < lenght; i++)
+            {
+                if (FirstLine[i] == SecondLine[i])
+                {
+                    countL++;
+
+                }
+            }
+            var y = 1;
+            for (int i = lenght; i > 0; i--)
+            {
+                 
+                if (FirstLine[FirstLine.Length-y] == SecondLine[SecondLine.Length-y])
+                {
+                    countR++;
+                     
+                }
+
+                y++;
+
+            }
+
+            if (countL > countR)
+            {
+                Console.WriteLine(countL);
+            }
+            else if (countR > countL)
+            {
+                Console.WriteLine(countR);
+            }
+            else
+            {
+                Console.WriteLine("0");
+            }
+
+
+
+
+
+        }
+    }
+}

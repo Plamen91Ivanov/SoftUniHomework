695b57ba1cc6ff55ae15ee972921efef4bb68052
plamen <plamen91ivanov@abv.bg>
2020-06-10 23:04:35 +0300

    ExamPrep

---
+++ C#/Fundamentals/ExamPreparation/BiscuitsFactory/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace BiscuitsFactory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int biscuitPerDay = int.Parse(Console.ReadLine());
+            int numberOfWorkers = int.Parse(Console.ReadLine());
+            int numberOfBiscuits = int.Parse(Console.ReadLine());
+
+            
+            double number = Math.Round((biscuitPerDay / 4.0),2)* numberOfWorkers;
+
+            double numb = (int)((0.25 * biscuitPerDay )* numberOfWorkers);
+
+            double evryThirdDay = (number * 10)  ;
+             
+            var numbersOfBiscuitsPerMonth = ((numberOfWorkers * biscuitPerDay) * 30) - evryThirdDay;
+
+            var produceCompared = numberOfBiscuits - numbersOfBiscuitsPerMonth; 
+
+            Console.WriteLine($"You have produced {numbersOfBiscuitsPerMonth} biscuits for the past month.");
+
+            var percentageDifference = (produceCompared / numberOfBiscuits) * 100;
+            var formatedProcentage =string.Format("{0:0.00}", percentageDifference); 
+
+            if (produceCompared < 0)
+            {
+                var absPercentage = Math.Abs(decimal.Parse(formatedProcentage));
+                Console.WriteLine($"You produce {absPercentage} percent more biscuits.");
+            }
+            else
+            {
+                var absPercentage = Math.Abs(decimal.Parse(formatedProcentage));
+                Console.WriteLine($"You produce {absPercentage} percent less biscuits.");
+            } 
+        }
+    }
+}
---
+++ C#/Fundamentals/ExamPreparation/Weaponsmith/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Weaponsmith
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine()
+               .Split('|') 
+               .ToList();
+            var oddList = new List<string>();
+            var evenList = new List<string>();
+
+            string command = Console.ReadLine();
+
+            while (command != "Done")
+            {
+                var parts = command
+                    .Split(" ") 
+                    .ToArray();
+                if (command == "Done")
+                {
+                    break;
+                }
+
+                string type = parts[0];
+                string direction = parts[1];
+                int position = 0;
+
+                if (parts.Count() == 3)
+                { 
+                     position = int.Parse(parts[2]);
+
+                }
+
+                if (type == "Move" && direction == "Left")
+                { 
+                    if (position > 0 && position < input.Count())
+                    { 
+                        var part = input.ElementAt(position);
+                          input.RemoveAt(position);
+                          input.Insert((position - 1), part);
+                    }
+                     
+                }
+                else if(type == "Move" && direction == "Right")
+                {
+                    if (position >= 0 && position < (input.Count()- 1))
+                    { 
+                         var part = input.ElementAt(position);
+                         input.RemoveAt(position);
+                         input.Insert((position + 1), part);
+                    } 
+                }
+                else if (command == "Check Even")
+                {
+                    for (int i = 0; i < input.Count; i++)
+                    {
+                        if (i % 2 == 0)
+                        {
+                            oddList.Add(input[i]);
+                        }
+                    }
+                    
+                }
+                else if(command == "Check Odd")
+                {
+                    for (int i = 0; i < input.Count; i++)
+                    {
+                        if (i % 2 != 0)
+                        {
+                            evenList.Add(input[i]);
+                        }
+                    }
+                     
+                }
+                 
+                command = Console.ReadLine(); 
+            } 
+
+            foreach(var item in oddList)
+            {
+                Console.Write(item + " ");
+            }
+
+            Console.WriteLine();
+
+            foreach (var item in evenList)
+            {
+                
+
+                Console.Write(item + " ",StringSplitOptions.RemoveEmptyEntries);
+            } 
+            Console.WriteLine(); 
+
+            Console.WriteLine($"You crafted {string.Join("",input)}!"); 
+        }
+    }
+}

2724c9061df49876bd4c2a34356e711bebb8ab3a
plamen <plamen91ivanov@abv.bg>
2020-06-18 16:29:43 +0300

    Classes

---
+++ C#/C#-Advance/Clases/DefineClass/Person.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DefineClass
+{
+    public class Person
+    {
+        private string name;
+        private int age;
+
+        public Person(string name , int age)
+        {
+            this.name = Name;
+            this.age = Age;
+        }
+
+        public string Name
+        {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+        public int Age
+        {
+            get { return this.age;}
+            set { this.age = value; }
+        }
+    }
+}
---
+++ C#/C#-Advance/Clases/DefineClass/StartUp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace DefineClass
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Person firstPerson = new Person();
+            Person secondPerson = new Person(2);
+            Person thrirdPerson = new Person("Pesho",30);
+            
+        }
+    }
+}

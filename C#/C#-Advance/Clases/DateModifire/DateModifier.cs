004db84b4484eaa8e97ad7a8d0255ddad543f6af
plamen <plamen91ivanov@abv.bg>
2020-06-18 18:10:42 +0300

    DateModifier

---
+++ C#/C#-Advance/Clases/DateModifire/DateModifier.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DateModifire
+{
+    public class DateModifier
+    {
+        public double DifferenceBetweenDates(string firstDate,string secondDate)
+        {
+            DateTime startDate = DateTime.Parse(firstDate);
+            DateTime endDate = DateTime.Parse(secondDate);
+
+            double result = Math.Abs((endDate - startDate).TotalDays);
+
+            return result;
+        }
+    }
+}
---
+++ C#/C#-Advance/Clases/DateModifire/StartUp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DateModifire
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string firstDate = Console.ReadLine();
+            string secondDate = Console.ReadLine();
+
+            DateModifier dateModifier = new DateModifier();
+            double result = dateModifier.DifferenceBetweenDates(firstDate, secondDate);
+
+            Console.WriteLine(result);
+        }
+    }
+}

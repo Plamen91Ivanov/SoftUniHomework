c4d700f55ef59be09038bad59c1cef1f530eaf76
plamen <plamen91ivanov@abv.bg>
2020-09-01 19:17:51 +0300

    Change style

--- JavaScript/DomManipulation/03. Accordion/accordion.js
+++ JavaScript/DomManipulation/03. Accordion/accordion.js
@@ -1,3 +1,15 @@
 function toggle() {
-    console.log('TODO:...');
+    
+    const $divs = document.querySelectorAll('div > div')
+    const $button = $divs[0].querySelector('span.button')
+    console.log($button)
+
+    if ($button.textContent === "More") {
+        $button.textContent = "Less";
+        $divs[1].style.display = 'block';
+    }else{
+        $button.textContent === "Less";
+        $divs[1].style.display = 'none';
+        $button.textContent = "More";
+    } 
 }

ce54b7ad3d111bc7dc50fdf6a64efd04ac40b7f6
plamen <plamen91ivanov@abv.bg>
2020-09-04 20:09:29 +0300

    Show/hide text

--- JavaScript/DomManipulation/07. Locked-Profile/app.js
+++ JavaScript/DomManipulation/07. Locked-Profile/app.js
@@ -1,3 +1,26 @@
 function lockedProfile() {
-    console.log('TODO...')
+        const $buttons = document.querySelectorAll('div#container main#main div.profile button');
+        
+        [...$buttons]
+        .forEach((button) => {
+            button.addEventListener('click', eventHandler);
+        }); 
+           
+        function eventHandler(event){
+
+          const divSibling = event.currentTarget.parentNode.children[9];
+          const selector = divSibling.id.split('HiddenFields')[0]
+          const $lockInput = document.querySelector(`input[name="${selector + 'Locked'}"]`);
+          
+          console.log($lockInput)
+
+        if (!$lockInput.checked && event.currentTarget.textContent === "Show more") {
+              divSibling.style.display = "block";
+              event.currentTarget.textContent = "Hide it";
+        }else if(!$lockInput.checked){
+            divSibling.style.display = "none";
+            event.currentTarget.textContent = "Show more";
+          } 
+ }
 }
+

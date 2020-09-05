d4767a43d4b4a83c69e8dd29f92fd930336f0bcc
plamen <plamen91ivanov@abv.bg>
2020-09-05 09:39:42 +0300

    lockedProfile

--- JavaScript/DomManipulation/08. Encode-and-Decode-Messages/app.js
+++ JavaScript/DomManipulation/08. Encode-and-Decode-Messages/app.js
@@ -1,4 +1,37 @@
 function encodeAndDecodeMessages() {
-   const $divs = document.querySelectorAll("div#container main#main div")
-   console.log($divs)
+    
+   const $textareas = document.querySelectorAll('textarea')
+    
+
+
+   $textareas[0].nextElementSibling.addEventListener('click', encode);
+   $textareas[1].nextElementSibling.addEventListener('click', decode);
+
+   console.log($textareas[0])
+   console.log($textareas[1])
+ 
+
+   function encode(){
+      const text = $textareas[0].value 
+         .split('').map((char) => {
+            return String.fromCharCode((char.charCodeAt(0) + 1));
+         })
+         .join(""); 
+
+         $textareas[0].value = "";
+         $textareas[1].value = text; 
+   }
+
+   function decode() {
+      let text = $textareas[1].value;
+
+      let decodeText = text.split('').map((char) => {
+         return String.fromCharCode((char.charCodeAt(0) - 1));
+      }).join("");
+      console.log(decodeText)
+
+      $textareas[1].value = decodeText;
+
+   }
+   
 }

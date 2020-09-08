2e2a55565324d96fc5fbb72aa32537db91567306
plamen <plamen91ivanov@abv.bg>
2020-09-08 09:43:32 +0300

    js

--- JavaScript/Dom/03. JavaScript-Quizz/solution.js
+++ JavaScript/Dom/03. JavaScript-Quizz/solution.js
@@ -1,3 +1,26 @@
 function solve() {
-  //TODO...
+  let quizzie = document.getElementById('quizzie')
+  let section = document.getElementsByTagName('section')
+
+  let correctAnswers = ['onclick','JSON.stringify()','HTML'];
+  let userAnswers = 0;
+
+  let currentStep = 0;
+
+    quizzie.addEventListener('click', (e) =>{
+
+    if(e.target.className === 'answer-text'){
+      console.log(e.target.innerHTML);
+
+      section[currentStep].style.display = 'block';
+
+      if(correctAnswers.some(answer => answer === e.target.innerHTML)){
+        userAnswers++;
+      }
+      
+      currentStep++;
+    }
+
+  });
+  
 }

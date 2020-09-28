1ec7ecbd8d0741ab408641a32bd0746b1c09b97e
plamen <plamen91ivanov@abv.bg>
2020-09-28 20:43:22 +0300

    js

--- JavaScript/MiniApps/testJs/script.js
+++ JavaScript/MiniApps/testJs/script.js
@@ -7,9 +7,34 @@
     console.log(userForm);
 
     userBtn.addEventListener('click', () => {
-        const createEL = document.createElement('div');
-        console.log(createEL)
-        userForm.innerHTML = `<h1>HIHIHIHI</h1>`
-     
-        console.log(userForm);
+
+        const myList = document.getElementById('test');
+
+        let newList = document.createElement('p');
+
+        newList.innerHTML = `<div class="col-md-4">
+        <form asp-route-returnUrl="@Model.ReturnUrl" method="post">
+            <h4>Create a new account.</h4>
+            <hr />
+            <div asp-validation-summary="All" class="text-danger"></div>
+            <div class="form-group">
+                <label asp-for="Input.Email"></label>
+                <input asp-for="Input.Email" class="form-control" />
+                <span asp-validation-for="Input.Email" class="text-danger"></span>
+            </div>
+            <div class="form-group">
+                <label asp-for="Input.Password"></label>
+                <input asp-for="Input.Password" class="form-control" />
+                <span asp-validation-for="Input.Password" class="text-danger"></span>
+            </div>
+            <div class="form-group">
+                <label asp-for="Input.ConfirmPassword"></label>
+                <input asp-for="Input.ConfirmPassword" class="form-control" />
+                <span asp-validation-for="Input.ConfirmPassword" class="text-danger"></span>
+            </div>
+            <button type="submit" class="btn btn-primary">Register</button>
+        </form>
+    </div>`;
+
+         myList.appendChild(newList);
     });

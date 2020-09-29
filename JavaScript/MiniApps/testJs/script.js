729b98a3670c3208b804e814c0310fed719261ae
plamen <plamen91ivanov@abv.bg>
2020-09-29 19:05:41 +0300

    test app Ui

--- JavaScript/MiniApps/testJs/script.js
+++ JavaScript/MiniApps/testJs/script.js
@@ -2,39 +2,26 @@
     var userBtn = document.getElementById('user-btn');
     var doctorBtn = document.getElementById('doctor-btn')
 
-    const userForm = document.getElementsByClassName('.row');
 
-    console.log(userForm);
 
     userBtn.addEventListener('click', () => {
 
-        const myList = document.getElementById('test');
+        const testEl = document.getElementById('user');
+ 
+        
+        location.reload();
+        
+        testEl.style.display = 'block';
+ 
+    });
 
-        let newList = document.createElement('p');
+    doctorBtn.addEventListener('click', () => {
 
-        newList.innerHTML = `<div class="col-md-4">
-        <form asp-route-returnUrl="@Model.ReturnUrl" method="post">
-            <h4>Create a new account.</h4>
-            <hr />
-            <div asp-validation-summary="All" class="text-danger"></div>
-            <div class="form-group">
-                <label asp-for="Input.Email"></label>
-                <input asp-for="Input.Email" class="form-control" />
-                <span asp-validation-for="Input.Email" class="text-danger"></span>
-            </div>
-            <div class="form-group">
-                <label asp-for="Input.Password"></label>
-                <input asp-for="Input.Password" class="form-control" />
-                <span asp-validation-for="Input.Password" class="text-danger"></span>
-            </div>
-            <div class="form-group">
-                <label asp-for="Input.ConfirmPassword"></label>
-                <input asp-for="Input.ConfirmPassword" class="form-control" />
-                <span asp-validation-for="Input.ConfirmPassword" class="text-danger"></span>
-            </div>
-            <button type="submit" class="btn btn-primary">Register</button>
-        </form>
-    </div>`;
+        const testEl = document.getElementById('doctor');
+ 
+        
+        location.reload(); 
+        
+        testEl.style.display = 'block';
 
-         myList.appendChild(newList);
     });
